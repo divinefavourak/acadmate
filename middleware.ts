531c3c6fb2a00549c588dc9dfb1282d300eeb2c9
divinefavourak <divinefavourakanbi07@gmail.com>
2026-05-08 09:49:20 +0100

    fix: update build command in render.yaml and enhance permissions in settings.local.json

--- middleware.ts
+++ middleware.ts
@@ -53,5 +53,5 @@ export default async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|images/).*)"],
 };
